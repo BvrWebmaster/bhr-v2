9807e2876ba005a50ecd378147855d019074b5c1
BvrWebmaster <webmaster1@bvrgroupasia.com>
2024-09-09 10:39:03 +0800

    update modal

--- resources/views/components/forms/login-form.blade.php
+++ resources/views/components/forms/login-form.blade.php
@@ -1,3 +1,22 @@
-<div>
-    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
+<div class="p-4 md:px-5 md:pt-2 md:pb-5">
+    <form method="post" class="space-y-4">
+        @csrf
+        @method("post")
+        <div>
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+            <input type="email" id="email" name="email" placeholder="name@gmail.com" required
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
+        </div>
+
+        <div>
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+            <input type="password" id="password" name="password" placeholder="••••••••" required
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
+        </div>
+
+        <button class="w-full text-white bg-[#FF5700] font-medium rounded-lg px-5 py-2.5 text-center transform duration-300 active:scale-95">
+            Log In
+        </button>
+    </form>
+
 </div>
