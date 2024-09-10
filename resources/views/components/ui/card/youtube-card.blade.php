b497a4b053fce828f114d97ce7cd205edc4b4765
BvrWebmaster <webmaster1@bvrgroupasia.com>
2024-09-10 12:51:28 +0800

    update youtube card

--- resources/views/components/ui/card/youtube-card.blade.php
+++ resources/views/components/ui/card/youtube-card.blade.php
@@ -1,5 +1,5 @@
 <div class="space-y-6">
-    <iframe class="rounded-xl w-[343px] h-[192px] tablet:w-[352px] tablet:h-[212px] laptop:w-[302px] laptop:h-[192px] laptop-l:w-[422px] laptop-l:h-[282px] desktop:w-[422px] desktop:h-[320px]" src="https://www.youtube.com/embed/{{ $embed }}" frameborder="0"></iframe>
+    <iframe class="rounded-xl w-[343px] h-[192px] tablet:w-[352px] tablet:h-[192px] laptop:w-[302px] laptop:h-[162px] laptop-l:w-[422px] laptop-l:h-[232px] desktop:w-[422px]" src="https://www.youtube.com/embed/{{ $embed }}" frameborder="0"></iframe>
     <div class="space-y-2">
         <p class="text-[#292929] text-base  laptop-l:text-xl font-semibold leading-[24px]">{{ $title }}</p>
         <div class="text-[#9E9E9E]">
