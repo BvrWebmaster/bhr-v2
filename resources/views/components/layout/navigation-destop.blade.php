04b42b4bfa9b314e7451c3188fb2335a8db2a815
BvrWebmaster <webmaster1@bvrgroupasia.com>
2024-09-12 10:17:06 +0800

    change id to class for call modal searching destop

--- resources/views/components/layout/navigation-destop.blade.php
+++ resources/views/components/layout/navigation-destop.blade.php
@@ -25,27 +25,27 @@
         <div class="flex justify-between pr-4">
             <div class="flex">
                 <div class="px-2 laptop-l:px-6 col-span-3 hover:bg-gray-200 py-2 laptop-l:pl-6 cursor-pointer transform duration-300 hover:rounded-2xl">
-                    <div class="laptop-l:w-[284px] space-y-2 container-location"  id="container-location-nav">
+                    <div class="laptop-l:w-[284px] space-y-2 container-location">
                         <h4 class="text-xs laptop-l:text-sm font-semibold leading-[21px] font-serif">Location</h4>
                         <p class="text-sm laptop-l:text-base font-medium leading-[24px] font-serif" id="input-location-nav">City, destination, or villa name</p>
                     </div>
                 </div>
 
                 <div class="px-2 laptop-l:px-6 hover:bg-gray-200 py-2 cursor-pointer transform duration-300 hover:rounded-2xl">
-                    <div class="space-y-2" id="container-start-date-nav">
+                    <div class="space-y-2 container-start-date">
                         <h4 class="text-xs laptop-l:text-sm font-semibold leading-[21px] font-serif">Check in</h4>
                         <p class="text-sm laptop-l:text-base font-semibold leading-[24px] font-serif" id="input-startDate-nav">startDate</p>
                     </div>
                 </div>
                 <div class="px-2 laptop-l:px-6 hover:bg-gray-200 py-2 cursor-pointer transform duration-300 hover:rounded-2xl">
-                    <div class="space-y-2" id="container-end-date-nav">
+                    <div class="space-y-2 container-end-date">
                         <h4 class="text-xs laptop-l:text-sm font-semibold leading-[21px] font-serif">Check out</h4>
                         <p class="text-sm laptop-l:text-base font-semibold leading-[24px] font-serif" id="input-endDate-nav">endDate</p>
                     </div>
                 </div>
 
                 <div class="px-2 laptop-l:px-6 hover:bg-gray-200 py-2 cursor-pointer transform duration-300 hover:rounded-2xl">
-                    <div class="space-y-2" id="container-guest-nav">
+                    <div class="space-y-2 container-guest">
                         <h4 class="text-xs laptop-l:text-sm font-semibold leading-[21px] font-serif">Guest</h4>
                         <p class="text-sm laptop-l:text-base text-[#888] font-semibold leading-[24px] font-serif" id="input-guest-nav">Add Guests</p>
                     </div>
@@ -53,7 +53,7 @@
             </div>
             <div class="col-span-2 flex items-center justify-center">
                 <a href="{{ route('hotels-and-villa.index') }}" class="flex items-center justify-center">
-                    <div class="px-4 bg-[#FF5700] rounded-xl py-2 laptop-l:px-6 transform duration-300 active:scale-95" id="btn-explore-nav">
+                    <div class="px-4 bg-[#FF5700] rounded-xl py-2 laptop-l:px-6 transform duration-300 active:scale-95">
                         <p class="text-base laptop-l:text-xl font-semibold font-serif leading-[30px] text-white">Search</p>
                     </div>
                 </a>
--- resources/views/components/layout/search-hotels-and-villa.blade.php
+++ resources/views/components/layout/search-hotels-and-villa.blade.php
@@ -2,27 +2,27 @@
    <div class="flex justify-between pr-4">
        <div class="flex">
            <div class="px-4 laptop-l:px-6 col-span-3 hover:bg-gray-200 py-3 pl-6 cursor-pointer transform duration-300 hover:rounded-2xl">
-               <div class="laptop-l:w-[284px]  space-y-2"  id="container-location-destop">
+               <div class="laptop-l:w-[284px]  space-y-2 container-location">
                    <h4 class="text-sm font-semibold leading-[21px] font-serif">Location</h4>
                    <p class="text-base laptop-l:text-lg font-medium leading-[24px] font-serif" id="input-location-desktop">City, destination, or villa name</p>
                </div>
            </div>
 
            <div class="px-4 laptop-l:px-6 hover:bg-gray-200 py-3 cursor-pointer transform duration-300 hover:rounded-2xl">
-               <div class="space-y-2" id="container-start-date-desktop">
+               <div class="space-y-2 container-start-date">
                    <h4 class="text-sm font-semibold leading-[21px] font-serif">Check in</h4>
                    <p class="text-base laptop-l:text-lg font-semibold leading-[24px] font-serif" id="input-startDate-desktop">startDate</p>
                </div>
            </div>
            <div class="px-4 laptop-l:px-6 hover:bg-gray-200 py-3 cursor-pointer transform duration-300 hover:rounded-2xl">
-               <div class="space-y-2" id="container-end-date-desktop">
+               <div class="space-y-2 container-end-date">
                    <h4 class="text-sm font-semibold leading-[21px] font-serif">Check out</h4>
                    <p class="text-base laptop-l:text-lg font-semibold leading-[24px] font-serif" id="input-endDate-desktop">endDate</p>
                </div>
            </div>
 
            <div class="px-4 laptop-l:px-6 hover:bg-gray-200 py-3 cursor-pointer transform duration-300 hover:rounded-2xl">
-               <div class="space-y-2" id="container-guest-desktop">
+               <div class="space-y-2 container-guest">
                    <h4 class="text-sm font-semibold leading-[21px] font-serif">Guest</h4>
                    <p class="text-base laptop-l:text-lg text-[#888] font-semibold leading-[24px] font-serif" id="input-guest-desktop">Add Guests</p>
                </div>
@@ -30,7 +30,7 @@
        </div>
        <div class="col-span-2 flex items-center justify-center">
            <a href="{{ route('hotels-and-villa.index') }}" class="flex items-center justify-center">
-               <div class="bg-[#FF5700] rounded-xl py-3 px-6 transform duration-300 active:scale-95" id="btn-explore">
+               <div class="bg-[#FF5700] rounded-xl py-3 px-6 transform duration-300 active:scale-95">
                    <p class="text-xl font-semibold font-serif leading-[30px] text-white">Search</p>
                </div>
            </a>
@@ -41,25 +41,21 @@
 
     <script>
         $(document).ready(function () {
-            $('#container-location-destop').click(function () {
-                $('#modal-searching').fadeIn('slow');
-            });
-
-            $('#container-start-date-desktop').click(function () {
-                $('#modal-searching').fadeIn('slow');
-            });
-
-            $('#container-end-date-desktop').click(function () {
-                $('#modal-searching').fadeIn('slow');
-            });
-
-            $('#container-guest-desktop').click(function () {
-                $('#modal-searching').fadeIn('slow');
-            });
-
             $('.container-location').click(function () {
-                $('#modal-searching').fadeIn('slow');
-            })
+                $('#modal-searching').fadeIn('fast');
+            });
+
+            $('.container-start-date').click(function () {
+                $('#modal-searching').fadeIn('fast');
+            });
+
+            $('.container-end-date').click(function () {
+                $('#modal-searching').fadeIn('fast');
+            });
+
+            $('.container-guest').click(function () {
+                $('#modal-searching').fadeIn('fast');
+            });
         });
     </script>
 </div>
