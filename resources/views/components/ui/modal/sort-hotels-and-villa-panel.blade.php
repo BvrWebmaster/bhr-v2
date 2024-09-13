3afdff05b8ab73f25139c2aae8fcb4e2060d6077
BvrWebmaster <webmaster1@bvrgroupasia.com>
2024-09-13 16:46:36 +0800

    add sort in panel mobile

--- resources/views/components/ui/modal/sort-hotels-and-villa-panel.blade.php
+++ resources/views/components/ui/modal/sort-hotels-and-villa-panel.blade.php
@@ -4,5 +4,20 @@
         <div id="btn-close-sort-hotels-and-villa">
             <x-ui.icon.close-icon-black />
         </div>
+
+        <x-layout.panel-mobile-layout>
+            <x-ui.label.text-header-filter-sort />
+            <div class="flex flex-col space-y-3 transition-max-height duration-700 ease-in-out" id="container-sort">
+                <div class="flex items-center space-x-3">
+                    <input type="radio" name="sort-recommended" class="rounded-xl accent-[#FF5700] h-3 w-3 cursor-pointer filter-location" id="mob-a-z" data-sort-id="asc"/>
+                    <label for="mob-a-z" class="text-[#292929] text-sm font-medium leading-[21px] cursor-pointer">A - Z</label>
+                </div>
+
+                <div class="flex items-center space-x-3">
+                    <input type="radio"  name="sort-recommended" class="rounded-xl accent-[#FF5700] h-3 w-3 cursor-pointer filter-location" id="mob-z-a" data-sort-id="desc"/>
+                    <label for="mob-z-a" class="text-[#292929] text-sm font-medium leading-[21px] cursor-pointer">Z - A</label>
+                </div>
+            </div>
+        </x-layout.panel-mobile-layout>
     </div>
 </div>
