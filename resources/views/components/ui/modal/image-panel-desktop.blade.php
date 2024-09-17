4c5ffe938a89af54ea24350cfa08c22798a567f8
BvrWebmaster <webmaster1@bvrgroupasia.com>
2024-09-17 15:35:20 +0800

    update image

--- resources/views/components/ui/modal/image-panel-desktop.blade.php
+++ resources/views/components/ui/modal/image-panel-desktop.blade.php
@@ -1,17 +1,17 @@
 <div class="hidden h-[100vh] w-full bg-black/70 fixed z-40 items-center justify-center" id="image-panel-desktop">
-    <div class="w-[75%] bg-black/90 h-[65vh] bg-white grid grid-cols-2 gap-2 rounded-xl overflow-y-hidden">
-        <div class="bg-white w-full h-full">
+    <div class="w-[75%] bg-black/90 h-[65vh] bg-white grid grid-cols-3 gap-2 rounded-xl overflow-y-hidden">
+        <div class="bg-white w-full h-full col-span-1">
             <div class="border-b border-r py-4 px-3">
                 <p class="text-xl">Galery</p>
             </div>
-            <div class="border-r grid grid-cols-2 h-[55vh] overflow-y-auto">
+            <div class="border-r grid grid-cols-2 h-[55vh] overflow-y-auto gap-2 px-4 py-2">
                 @foreach($images as $image)
                     <img src="{{ $image->featured_image }}" alt="{{ $image->name }}" class="h-full w-full images-list cursor-pointer" data-images-panel="{{ $image }}">
                 @endforeach
             </div>
         </div>
 
-        <div class="w-full h-full py-4 ">
+        <div class="w-full h-full py-4 col-span-2">
             <div class="w-full flex justify-end px-4 py-2">
                 <div id="close-image-panel">
                     <x-ui.icon.close-icon />
