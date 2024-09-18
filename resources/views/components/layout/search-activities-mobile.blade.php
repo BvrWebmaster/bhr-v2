b100f904f28b094d528dd274417c606c23e56589
BvrWebmaster <webmaster1@bvrgroupasia.com>
2024-09-18 17:24:47 +0800

    add search mobile

--- resources/views/components/layout/search-activities-mobile.blade.php
+++ resources/views/components/layout/search-activities-mobile.blade.php
@@ -3,8 +3,38 @@
         <label for="search-activities">
             <x-ui.icon.search-icon />
         </label>
-        <input type="text" id="search-activities" placeholder="Search Activities"  class="w-full text-[#989898] font-medium text-base  leading-[24px] focus:outline-none"/>
+        <input type="text" id="input-activities-mobile" placeholder="Search Activities"  class="w-full text-[#989898] font-medium text-base  leading-[24px] focus:outline-none"/>
     </x-layout.input-search-mobile>
 
-    <x-ui.button.button-search-mobile label="Search" />
+    <button  id="btn-search-mobile" class="transform duration-300 active:scale-95 border bg-[#FF5700] rounded-xl w-full py-3 text-white text-base font-semibold leading-[24px]">
+        Search
+    </button>
+
+    <script>
+        $(document).ready(function () {
+            $('#btn-search-mobile').on('click', function () {
+                const inputSearch = $('#input-activities-mobile').val();
+
+                $.ajax({
+                    url: "{{ route('activities.list') }}",
+                    method: "GET",
+                    data: {
+                        search: inputSearch,
+                        page: 1
+                    },
+                    success: function (response) {
+                        $('#activities-page-main').empty();
+
+                        $.each(response.data, function (index, activities) {
+
+                            $('#activities-page-main').append(cardActivitiesMain(activities));
+                        });
+                    },
+                    error: function (xhr) {
+                        console.log(xhr);
+                    }
+                });
+            })
+        });
+    </script>
 </div>
