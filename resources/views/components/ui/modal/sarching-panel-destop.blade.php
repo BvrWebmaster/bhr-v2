12d7892927f5b76c99b9bde317b814c838d67405
BvrWebmaster <webmaster1@bvrgroupasia.com>
2024-10-16 14:34:39 +0800

    update sarching panel destop bg

--- resources/views/components/ui/modal/sarching-panel-destop.blade.php
+++ resources/views/components/ui/modal/sarching-panel-destop.blade.php
@@ -1,9 +1,9 @@
 <div id="modal-searching" class="hidden">
     <div class="overflow-y-auto overflow-x-hidden fixed top-0 z-50 flex justify-center items-center w-full tablet:inset-0 bg-black/50 backdrop-blur h-[100vh]">
         <div class="relative w-full bg-white max-w-md max-h-full transform duration-200 space-y-4">
-            <div class="relative rounded-lg shadow dark:bg-gray-700 py-2 px-4">
-                <div class="flex items-center justify-between py-2 border-b rounded-t dark:border-gray-600">
-                    <button id="btn-modal-searching" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            <div class="relative rounded-lg shadow py-2 px-4">
+                <div class="flex items-center justify-between py-2 border-b rounded-t">
+                    <button id="btn-modal-searching" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
                             data-modal-hide="authentication-modal">
                         <x-ui.icon.close-icon-black />
                     </button>
@@ -13,26 +13,26 @@
 
                     <!-- container load location -->
                     <div class="space-y-2">
-                        <label for="container-locations-desktop" class="block text-sm font-medium text-gray-900 dark:text-white">Select Location</label>
-                        <select id="container-locations-desktop" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <label for="container-locations-desktop" class="block text-sm font-medium text-gray-900">Select Location</label>
+                        <select id="container-locations-desktop" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                         </select>
                     </div>
 
                     <!-- input datetime -->
                     <div  class="w-full flex items-center justify-between tablet:justify-start tablet:space-x-2">
                         <div class="relative w-full">
-                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start date</label>
-                            <input id="start-date-desktop" name="start" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date start">
+                            <label class="block mb-2 text-sm font-medium text-gray-900">Start date</label>
+                            <input id="start-date-desktop" name="start" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5" placeholder="Select date start">
                         </div>
                         <div class="relative w-full">
-                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">End date</label>
-                            <input id="end-date-desktop" name="start" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date start">
+                            <label class="block mb-2 text-sm font-medium text-gray-900">End date</label>
+                            <input id="end-date-desktop" name="start" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5" placeholder="Select date start">
                         </div>
                     </div>
 
                     <!-- input guest -->
                     <div>
-                        <label for="container-locations-mobile" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Guest</label>
+                        <label for="container-locations-mobile" class="block mb-2 text-sm font-medium text-gray-900">Select Guest</label>
                         <x-ui.partials.guest-select
                             label="Adult"
                             subLabel="Ages 17 or above"
